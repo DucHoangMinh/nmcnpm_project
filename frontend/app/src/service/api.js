0bee416575484a49f59f1fcd7eb9c30157891466
vusociu <vu13102003@gmail.com>
2023-11-12 18:15:02 +0700

    fix dataToken

--- frontend/app/src/service/api.js
+++ frontend/app/src/service/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-const dataToken = JSON.parse(localStorage.getItem('tokenData'))
+const dataToken = JSON.parse(localStorage.getItem('tokenData'))||{}
 const headers = {
     'Content-Type': 'application/json',
-    'Authorization': dataToken.tokenType || '' +' '+dataToken.token||''
+    'Authorization': `${dataToken.tokenType??''} ${dataToken.token??''}`
 }
 const api = {
-    post : async (url, body) => {
+    post : async (url, body) => {   
         return await axios.post('http://localhost:8080/api/' + url, body, {headers: headers})
     }
 }
