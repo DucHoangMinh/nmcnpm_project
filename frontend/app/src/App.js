c02420b2f556f1d00d4004b496fd2622dedcacae
vusociu <vu13102003@gmail.com>
2023-12-22 14:24:26 +0700

    commit

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
-import { Fragment } from 'react';
 import { FeeRoute } from "./admin/modules/fee/route";
 import {AdminAuthorizeRoute} from "./admin/modules/authorize/route";
 import { UserRoute } from './admin/modules/user/route';
--- frontend/app/src/admin/modules/user/route.js
+++ frontend/app/src/admin/modules/user/route.js
@@ -1,15 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import FamilyList from './jsx/list';
-import RoomProssess from './jsx/process';
-import RoomList from './jsx/roomlist';
-const FamilyRoute = () =>{
+import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
+import Room from './jsx/room';
+import ManageIndex from './jsx';
+const UserRoute = () =>{
     return (
         <Routes>
-            <Route path="/family/list" element={<FamilyList/>} />
-            <Route path="/family/room-request" element={<RoomProssess/>} />
-            <Route path="/family/room-list" element={<RoomList/>} />
+            <Route path="manage-user/*" element={
+                <Routes>
+                    <Route path="" element = {<ManageIndex/>}/>
+                    <Route path="room/:id" element = {<Room/>}/>
+                </Routes>
+            } />
         </Routes>
     );
 }
 
-export {FamilyRoute};
+export {UserRoute};
